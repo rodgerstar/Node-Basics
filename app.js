fe475339c89aa39726ec93e2e1fa6ed38cce04d5
Rodgers Okwemba <rodgerstarrodgers@gmail.com>
2025-09-28 00:58:59 +0300

    Events and events arguments

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 
 
-function sayHello(name) {
-    console.log('Hello ' + name);
-} 
-sayHello('Mosh')
+// function sayHello(name) {
+//     console.log('Hello ' + name);
+// } 
+
+// sayHello('Mosh')
+
+
+
+
+
+//Events
+
+// const CheckEvent = require('events');
+// const emitter = new CheckEvent();
+
+// //Register a listener
+// emitter.on('messageLogged', function(){
+//     console.log('Here There Am Alive');
+// });
+
+// //Raise/Call an event
+// emitter.emit('messageLogged');
+
+
+//Event Argument
+
+const CheckEvent = require('events');
+const emitter = new CheckEvent();
+
+//Register a listener
+emitter.on('messageLogged', (arg) => {
+    console.log('Here There Am Alive', arg);
+});
+
+//Raise/Call an event
+emitter.emit('messageLogged', {id: 1, url: 'http://' });
